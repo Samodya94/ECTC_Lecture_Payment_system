20d0340c9d518c71a72061e56e00f9949a12348b
sharmelang <130053966+sharmelang@users.noreply.github.com>
2023-09-08 10:04:48 +0530

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv =require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
+const { protectUser } = require('./middleware/authMiddleware');
+const cors = require('cors');
 const port = process.env.PORT || 5001;
 const connectDB = require('./config/db');
 
@@ -10,11 +12,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(cors());
+
 app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/branch', require('./routes/branchRoutes'));
-app.use('/api/batch', require('./routes/batchRoutes'));
-app.use('/api/course', require('./routes/courseRoutes'));
-app.use('/api/lecturer', require('./routes/lecturerRoutes'));
+app.use('/api/branch', protectUser, require('./routes/branchRoutes'));
+app.use('/api/batch', protectUser, require('./routes/batchRoutes'));
+app.use('/api/course', protectUser, require('./routes/courseRoutes'));
+//app.use('/api/lecturer', protectUser, require('./routes/lecturerRoutes'));
+app.use('/api/coverage', protectUser, require('./routes/approveLectureCoverageRoutes'));
 
 app.use(errorHandler);
 

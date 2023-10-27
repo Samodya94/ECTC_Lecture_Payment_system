c3a5799dc564811a2da2833b4e9aa067a6e25476
Kalana-Ranahansa <kalanar@ewisl.net>
2023-10-27 14:05:28 +0530

    user logout

--- client/src/Pages/Admin/header/components/dropdownCard.jsx
+++ client/src/Pages/Admin/header/components/dropdownCard.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { useNavigate } from "react-router";
 
+import { useLogout } from "../../../../hooks/useLogout";
+
 // Styles
 import styles from "./dropdownCard.module.css";
+
+import Cookies from "js-cookie";
 
 // MUI icons
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import LogoutIcon from "@mui/icons-material/Logout";
 
+
 const DropdownCard = () => {
   const navigate = useNavigate();
+  const { logout } = useLogout();
+
+  const handleLogout = () => {
+
+    logout()
+    // alert('Logged out');
+    navigate('/admin-login')
+
+  }
 
   return (
     <div className={styles.dropdownCard}>
@@ -22,7 +36,7 @@ const DropdownCard = () => {
             <span className={styles.span}>Profile</span>
           </span>
         </li>
-        <li className={styles.li} onClick={() => navigate("approve-lectures")}>
+        <li className={styles.li} onClick={handleLogout}>
           <span className={styles.liContainer}>
             <LogoutIcon />
             <span className={styles.span} style={{ whiteSpace: "nowrap" }}>
--- client/src/hooks/useLogout.jsx
+++ client/src/hooks/useLogout.jsx
@@ -1,18 +1,21 @@
 import { useAuthContext } from "./useAuthContext";
+import { useNavigate } from "react-router";
 import Cookies from "js-cookie";
 
 export const useLogout = () => {
+    const navigate = useNavigate()
     const { dispatch } = useAuthContext()
 
     const logout = () => {
+        //remove the user from local storage
+        Cookies.remove('user')
+        Cookies.remove('username')
+        Cookies.remove('userLevel')
+        Cookies.remove('token')
 
-        Cookies.remove('username'),
-            Cookies.remove('token'),
-            Cookies.remove('userLevel'),
-            Cookies.remove('admin'),
+        dispatch({ type: 'LOGOUT' })
 
-
-            dispatch({ type: 'LOGOUT' })
+        navigate('/admin-login');
 
     }
 

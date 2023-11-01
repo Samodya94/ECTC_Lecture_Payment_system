6f171ec8346700e82cf1bdde8c2eda0fb0257db6
Kalana-Ranahansa <kalanar@ewisl.net>
2023-11-01 10:11:39 +0530

    Update branchRoutes.js

--- server/routes/branchRoutes.js
+++ server/routes/branchRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getBranch, createBranch, deleteBranch } = require('../controllers/branchController');
+const { getBranch, createBranch, deleteBranch, getallBranches } = require('../controllers/branchController');
 const { authorize } = require('../middleware/authMiddleware');
 
 const branchAccessControl = authorize(["Admin"]); // Adjust user levels as needed
 
 router.route('/').get(branchAccessControl, getBranch).post(branchAccessControl, createBranch);
 router.route('/:id').delete(branchAccessControl, deleteBranch);
+router.route('/all').get(branchAccessControl, getallBranches);
 
 module.exports = router;

870838613636654b60c065659fcac9a89f462ef4
Kalana-Ranahansa <kalanar@ewisl.net>
2023-11-08 12:58:44 +0530

    Update courseRoutes.js

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createCourse, deleteCourse, putCourse, getCourse, getCourseNames, getallCourses } = require('../controllers/courseController');
+const { createCourse, deleteCourse, putCourse, getCourse, getCourseByName, getallCourses } = require('../controllers/courseController');
 const { authorize } = require('../middleware/authMiddleware');
 
 const branchAccessControl = authorize(["Admin", "Manager"]);
 
 router.post('/', branchAccessControl, createCourse).get('/', branchAccessControl, getCourse);
 router.put('/:id', branchAccessControl, putCourse).delete('/:id', branchAccessControl, deleteCourse);
-router.get('/courseName', branchAccessControl, getCourseNames);
-router.get('/all', branchAccessControl, getallCourses);
+router.get('/:courseName', branchAccessControl, getCourseByName);
 
 module.exports = router;

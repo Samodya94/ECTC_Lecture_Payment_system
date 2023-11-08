9855da59ee817382eaa5417931696a000142c8d7
Kalana-Ranahansa <kalanar@ewisl.net>
2023-11-08 10:38:04 +0530

    update userLevel for data fetching

--- server/routes/batchRoutes.js
+++ server/routes/batchRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { createBatch, putBatch, deleteBatch, getBatch, getallBatches, getBatchById } = require('../controllers/batchController');
 const { authorize } = require('../middleware/authMiddleware');
 
-const branchAccessControl = authorize(["Admin"]);
+const branchAccessControl = authorize(["Admin", "Manager"]);
 
 router.post('/', branchAccessControl, createBatch).get('/', branchAccessControl, getBatch);
 router.put('/:id', branchAccessControl, putBatch).delete('/:id', branchAccessControl, deleteBatch);
--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { createCourse, deleteCourse, putCourse, getCourse, getCourseNames, getallCourses } = require('../controllers/courseController');
 const { authorize } = require('../middleware/authMiddleware');
 
-const branchAccessControl = authorize(["Admin"]);
+const branchAccessControl = authorize(["Admin", "Manager"]);
 
 router.post('/', branchAccessControl, createCourse).get('/', branchAccessControl, getCourse);
 router.put('/:id', branchAccessControl, putCourse).delete('/:id', branchAccessControl, deleteCourse);

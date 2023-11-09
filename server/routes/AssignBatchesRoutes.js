69e697ef175eb7adba26163f5f36b5aaca38e6df
Kalana-Ranahansa <kalanar@ewisl.net>
2023-11-09 14:14:51 +0530

    Update AssignBatchesRoutes.js

--- server/routes/AssignBatchesRoutes.js
+++ server/routes/AssignBatchesRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { AssignBatch ,putAssignBatch, deleteAssignedBatch } = require('../controllers/assignbatchController');
+const { AssignBatch, putAssignBatch, deleteAssignedBatch } = require('../controllers/assignbatchController');
+const { authorize } = require('../middleware/authMiddleware');
 
-router.post('/', AssignBatch);
-router.put('/:id', putAssignBatch);
-router.delete('/:id', deleteAssignedBatch);
+const branchAccessControl = authorize(["Manager"]);
+
+router.post('/', branchAccessControl, AssignBatch);
+router.put('/:id', branchAccessControl, putAssignBatch);
+router.delete('/:id', branchAccessControl, deleteAssignedBatch);
 
 module.exports = router;

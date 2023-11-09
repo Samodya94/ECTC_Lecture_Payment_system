361525dd75c68601e7123854bef69d377bb3f7b3
Kalana-Ranahansa <kalanar@ewisl.net>
2023-11-09 14:14:31 +0530

    Update assignbatchController.js

--- server/controllers/assignbatchController.js
+++ server/controllers/assignbatchController.js
@@ -1,41 +1,45 @@
 const asyncHandler = require('express-async-handler');
 const AssignedBatch = require('../model/assignbatchmodel');
 
-const AssignBatch = asyncHandler(async (req,res) => {
-    const { lecturer, batchcode, rate, hours } = req.body;
-    console.log(lecturer, batchcode, rate, hours);
+const AssignBatch = asyncHandler(async (req, res) => {
+    const { lecturerNic, lecturerName, course, batchcode, rate, hours } = req.body;
+    console.log(lecturerNic, lecturerName, course, batchcode, rate, hours);
 
-    if(!lecturer || !batchcode || !rate || !hours) {
+    if (!lecturerNic || !lecturerName || !course || !batchcode || !rate || !hours) {
         res.status(400);
         throw new Error('Please Fill All Fields');
     }
 
     const batchAssigned = await AssignedBatch.findOne({ batchcode });
 
-    if(batchAssigned) {
+    if (batchAssigned) {
         res.status(400);
         throw new Error('Batch Already Assigned');
     }
 
     const abatch = await AssignedBatch.create({
-        lecturer,
+        lecturerNic,
+        lecturerName,
+        course,
         batchcode,
         rate,
         hours,
     });
 
-    if(abatch) {
+    if (abatch) {
         res.status(200).json({
             _id: abatch.id,
-            lecturer: abatch.lecturer,
-            batchcode : abatch.batchcode,
+            lecturerNic: abatch.lecturerNic,
+            lecturerName: abatch.lecturerName,
+            course: abatch.course,
+            batchcode: abatch.batchcode,
             rate: abatch.rate,
             hours: abatch.hours,
-            
+
         });
     } else {
-            res.status(400);
-            throw new Error('Invalid Details');
+        res.status(400);
+        throw new Error('Invalid Details');
     }
 
     res.json({ message: 'Batch Assigned' });

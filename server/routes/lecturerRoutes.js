bc83370da19c90639159b59a89d58fce9878927e
Samodya94 <samodyan@ewisl.net>
2023-11-16 10:13:57 +0530

    lecture login backend

--- server/routes/lecturerRoutes.js
+++ server/routes/lecturerRoutes.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
 const router = express.Router();
-const { getLecturer, createLecturer, deleteLecturer, putLecturer, getLecturerById } = require('../controllers/lecturerController');
+const { getLecturer, createLecturer, deleteLecturer, putLecturer, getLecturerById, loginLecturer } = require('../controllers/lecturerController');
 
 router.post('/', createLecturer);
 router.get('/', getLecturer);
+router.post('/login',loginLecturer)
 router.put('/:id', putLecturer);
 router.delete('/:id', deleteLecturer);
 router.get('/:id', getLecturerById);

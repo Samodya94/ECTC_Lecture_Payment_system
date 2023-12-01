90d5e38e9512c9fad407c8977cfa6831fad87210
Kalana-Ranahansa <kalanar@ewisl.net>
2023-12-01 10:12:07 +0530

    Update approveLectureCoverageRoutes.js

--- server/routes/approveLectureCoverageRoutes.js
+++ server/routes/approveLectureCoverageRoutes.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { createCoverage,
-        deleteCoverage,
-        putCoverage,
-        getCoverage,
-        getCoverages,
-        getCoverageNotApproved,
-        getCoverageApproved,
-        getLecCoverageNotApproved,
-        getCoverageNotApprovedByMonth,
-        getCoverageApprovedByLecturer,
-        getCoverageByID } = require('../controllers/approveLectureCoversgeController');
+    deleteCoverage,
+    putCoverage,
+    getCoverage,
+    getCoverageById,
+    getCoverageNotApproved,
+    getCoverageApproved,
+    getLecCoverageNotApproved,
+    getCoverageNotApprovedByMonth,
+    getCoverageApprovedByLecturer } = require('../controllers/approveLectureCoversgeController');
 
 router.post('/', createCoverage).get('/', getCoverage);
 router.put('/:id', putCoverage).delete('/:id', deleteCoverage);
-router.get('/:id', getCoverages);
 router.get('/notapproved', getCoverageNotApproved);
 router.get('/approved', getCoverageApproved);
+router.get('/:id', getCoverageById);
 router.get('/approved/:lecid', getCoverageApprovedByLecturer)
 router.get('/lecnotApproved/:lecid', getLecCoverageNotApproved);
 router.get('/notapprovedbymonth/:lecid', getCoverageNotApprovedByMonth)

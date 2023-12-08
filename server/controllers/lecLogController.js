76d76f6b4b12186bba83bc3cb3978d9cbe62fd0f
Samodya94 <samodyan@ewisl.net>
2023-12-08 13:16:35 +0530

    adding get logDetails

--- server/controllers/lecLogController.js
+++ server/controllers/lecLogController.js
@@ -18,10 +18,19 @@ const createLog = asyncHandler(async (req,res) =>{
 const getLecLog = asyncHandler(async(req,res) =>{
     const lecLog = await LecLog.find()
 
-    res(200).json(lecLog)
+    res.status(200).json(lecLog)
+})
+
+const getLogByUser = asyncHandler(async(req,res)=>{
+    const user = req.params.user
+
+    const leclog = await LecLog.find({ lecUsername:user})
+
+    res.status(200).json(leclog)
 })
 
 module.exports = {
     createLog,
-    get
+    getLecLog,
+    getLogByUser
 }
--- server/routes/lecturerlogRoutes.js
+++ server/routes/lecturerlogRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {
-    createLog
+    createLog,
+    getLecLog,
+    getLogByUser
 } = require('../controllers/lecLogController')
 
 router.post('/',createLog)
+router.get('/',getLecLog)
+router.get('/:user',getLogByUser)
 
 module.exports = router

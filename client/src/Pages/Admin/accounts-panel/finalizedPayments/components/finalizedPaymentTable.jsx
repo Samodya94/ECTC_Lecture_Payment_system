cf2e74ea37d738a0d7f99a2e251782ba679c6264
Kalana-Ranahansa <kalanar@ewisl.net>
2023-12-08 12:31:32 +0530

    fetch approvedPayment data

--- client/src/Pages/Admin/accounts-panel/finalizedPayments/components/finalizedPaymentTable.jsx
+++ client/src/Pages/Admin/accounts-panel/finalizedPayments/components/finalizedPaymentTable.jsx
@@ -26,6 +26,8 @@ import LastPageIcon from "@mui/icons-material/LastPage";
 // Styles
 import styles from "./finalizedPaymentTable.module.css";
 
+import Service from "../../../../../utilities/httpService";
+
 function TablePaginationActions(props) {
   const theme = useTheme();
   const { count, page, rowsPerPage, onPageChange } = props;
@@ -111,6 +113,75 @@ const TableComponent = ({ rows, columns }) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
+
+  function calculateDuration(duration) {
+    const hours = Math.floor(duration / 3600000);
+    const minutes = Math.floor((duration % 3600000) / 60000);
+
+    return `${hours}h : ${minutes}m`;
+  }
+
+  const service = React.useMemo(() => new Service(), []);
+
+  //get batch batchCode from coverage batchCode
+  const [batched, setBatched] = React.useState({});
+
+  React.useEffect(() => {
+    const getBatch = async () => {
+      const response = await service.get("assignbatch");
+      const batches = response.data.reduce((acc, batch) => {
+        acc[batch._id] = batch.batchCode;
+        return acc;
+      }, {});
+      setBatched(batches);
+    };
+
+    getBatch();
+  }, [rows, service]);
+
+  //get batch batchCode from batched batchCode
+  const [batchCodes, setBatchCodes] = React.useState({});
+
+  React.useEffect(() => {
+    const getBatchCode = async () => {
+      const response = await service.get("batch");
+      const batches = response.data.reduce((acc, batch) => {
+        acc[batch._id] = batch.batchCode;
+        return acc;
+      }, {});
+      setBatchCodes(batches);
+    };
+
+    getBatchCode();
+  }, [rows, service]);
+
+  const [lecturerNames, setLecturerNames] = React.useState({});
+
+  React.useEffect(() => {
+    const getLecturerName = async (id) => {
+      try {
+        const response = await service.get(`lecturer/${id}`);
+        const lecturerName = response.data.firstName + " " + response.data.lastName;
+        setLecturerNames((prevNames) => ({
+          ...prevNames,
+          [id]: lecturerName,
+        }));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLecturerNames((prevNames) => ({
+          ...prevNames,
+          [id]: "", // Set an empty string or handle the error as needed
+        }));
+      }
+    };
+
+    rows.forEach((row) => {
+      if (!lecturerNames[row.lecturerId]) {
+        getLecturerName(row.lecturerId);
+      }
+    });
+  }, [rows, lecturerNames, service]);
+
   return (
     <>
       <TableContainer
@@ -122,6 +193,7 @@ const TableComponent = ({ rows, columns }) => {
             <TableRow className={styles.tHead}>
               {columns.map((column, index) => (
                 <TableCell
+                  key={index}
                   style={{ border: "1px solid #ccc", padding: "8px 16px" }}
                 >
                   <span className={styles.tHead}>{column}</span>
@@ -133,8 +205,8 @@ const TableComponent = ({ rows, columns }) => {
             {(rowsPerPage > 0
               ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               : rows
-            ).map((row) => (
-              <TableRow key={row.name} style={{ border: "1px solid #ccc" }}>
+            ).map((row, _id) => (
+              <TableRow key={row._id} style={{ border: "1px solid #ccc" }}>
                 <TableCell
                   component="th"
                   scope="row"
@@ -143,7 +215,7 @@ const TableComponent = ({ rows, columns }) => {
                     padding: "5px 16px",
                   }}
                 >
-                  {row.lecturerName}
+                  {lecturerNames[row.lecturerId] || "Loading..."}
                 </TableCell>
                 <TableCell
                   style={{
@@ -153,7 +225,7 @@ const TableComponent = ({ rows, columns }) => {
                   }}
                   align="left"
                 >
-                  {row.courseName}
+                  {row.coursename}
                 </TableCell>
                 <TableCell
                   style={{
@@ -163,7 +235,7 @@ const TableComponent = ({ rows, columns }) => {
                   }}
                   align="left"
                 >
-                  {row.batchCode}
+                  {batchCodes[batched[row.batchcode]]}
                 </TableCell>
                 <TableCell
                   style={{
@@ -173,7 +245,7 @@ const TableComponent = ({ rows, columns }) => {
                   }}
                   align="left"
                 >
-                  {row.Month}
+                  {row.month}
                 </TableCell>
                 <TableCell
                   style={{
@@ -183,7 +255,7 @@ const TableComponent = ({ rows, columns }) => {
                   }}
                   align="left"
                 >
-                  {row.totalHours}
+                  {calculateDuration(row.totalhours)}
                 </TableCell>
                 <TableCell
                   style={{
@@ -193,7 +265,7 @@ const TableComponent = ({ rows, columns }) => {
                   }}
                   align="left"
                 >
-                  {row.payRate}
+                  {row.paymentrate}
                 </TableCell>
                 <TableCell
                   style={{
@@ -203,7 +275,7 @@ const TableComponent = ({ rows, columns }) => {
                   }}
                   align="left"
                 >
-                  RS. {row.payAmount}
+                  RS. {row.paidamount}
                 </TableCell>
                 <TableCell
                   style={{

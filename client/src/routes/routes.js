553e8a0cf0715531d8a9746a0674f36ec6e81221
Kalana-Ranahansa <kalanar@ewisl.net>
2023-12-08 12:04:35 +0530

    update routes

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useLecAuthContext } from "../hooks/useLecAuthContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // Components
 import App from "../App";
@@ -14,20 +15,22 @@ import LectureLogin from "../Pages/Lecturer/Login/LectureLogin";
 
 const AllRoutes = () => {
 
-  const {lecturer} = useLecAuthContext();
+  const { lecturer } = useLecAuthContext();
+  const { user } = useAuthContext();
+
   return (
     <Router>
       <Routes>
         {/* Unprotected Routes */}
-        <Route path="/admin-login" element={<AdminLogin />} />
-        <Route path="/lec-login" element={!lecturer ? <LectureLogin/> : <Navigate to="/lecture"/>}/>
-        
+        <Route path="/" element={!user ? <AdminLogin /> : <Navigate to="/admin/dashboard" />} />
+        <Route path="/lec-login" element={!lecturer ? <LectureLogin /> : <Navigate to="/lecture" />} />
+
         {/* Protected Routes */}
         <Route path="/" element={<App />}>
-          <Route path="/admin/*" element={<LandingPage />} />
-          <Route path="/lecture/*" element={lecturer ? <LecHome/> : <Navigate to="/lec-login"/>}/>
+          <Route path="/admin/*" element={user ? <LandingPage /> : <AdminLogin />} />
+          <Route path="/lecture/*" element={lecturer ? <LecHome /> : <Navigate to="/lec-login" />} />
         </Route>
-        
+
       </Routes>
     </Router>
   );

3f6f0e8220b6020de54f81766eac42b49563a505
Kalana-Ranahansa <kalanar@ewisl.net>
2023-12-12 11:15:04 +0530

    Update routes.js

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -22,13 +22,13 @@ const AllRoutes = () => {
     <Router>
       <Routes>
         {/* Unprotected Routes */}
-        <Route path="/" element={!user ? <AdminLogin /> : <Navigate to="/admin/dashboard" />} />
+        <Route path="/" element={!user ? <AdminLogin /> : <Navigate to="/admin" />} />
         <Route path="/lec-login" element={!lecturer ? <LectureLogin /> : <Navigate to="/lecture" />} />
 
         {/* Protected Routes */}
         <Route path="/" element={<App />}>
-          <Route path="/admin/*" element={user ? <LandingPage /> : <AdminLogin />} />
-          <Route path="/lecture/*" element={lecturer ? <LecHome /> : <Navigate to="/lec-login" />} />
+          <Route path="/admin/*" element={user ? <LandingPage /> : <Navigate to="/" replace />} />
+          <Route path="/lecture/*" element={lecturer ? <LecHome /> : <Navigate to="/lec-login" replace />} />
         </Route>
 
       </Routes>

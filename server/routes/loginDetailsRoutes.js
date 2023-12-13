2fe32857e8cc2cc80f060b80c26061d03a2df2b5
Kalana-Ranahansa <kalanar@ewisl.net>
2023-12-13 23:28:42 +0530

    get recent log details

--- server/routes/loginDetailsRoutes.js
+++ server/routes/loginDetailsRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { getLogByUser,
     getLoginDetails,
-    createLog } = require('../controllers/loginDetailsController');
+    createLog,
+    getRecentLogByUser } = require('../controllers/loginDetailsController');
 
 router.post('/', createLog).get('/', getLoginDetails);
 router.get('/:username', getLogByUser);
+router.get('/recent/:username', getRecentLogByUser);
 
 module.exports = router;

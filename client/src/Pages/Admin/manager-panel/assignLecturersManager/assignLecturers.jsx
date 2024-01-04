43d176db178a2b12cad64d723dc63eeceb0111ef
Kalana-Ranahansa <kalanar@ewisl.net>
2024-01-04 12:08:11 +0530

    add enddate to assignbatch

--- client/src/Pages/Admin/manager-panel/assignLecturersManager/assignLecturers.jsx
+++ client/src/Pages/Admin/manager-panel/assignLecturersManager/assignLecturers.jsx
@@ -128,6 +128,22 @@ const AssignLecturers = () => {
     }
   }
 
+  //get end date from batch code
+  async function getEndDate(batchCode) {
+    try {
+      let endDate = "";
+      batchCodeList.forEach((item) => {
+        if (item._id === batchCode) {
+          endDate = item.endDate;
+        }
+      });
+      return endDate;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+
   const handleOptionChange = (e) => {
     setLecturer(e.target.value);
   };
@@ -173,6 +189,8 @@ const AssignLecturers = () => {
     // Move the calls inside the function
     const lecturerNic = getLecturerNic(lecturerName);
     const course = getCourse(batchCode);
+    const endDate = getEndDate(batchCode);
+    console.log(lecturerNic, course, endDate);
     const _id = getLecturerId(lecturerName);
 
     // Wait for both promises to resolve
@@ -188,6 +206,7 @@ const AssignLecturers = () => {
           hours: noOfHours * 3600000,
           remaining_hours: noOfHours * 3600000,
           hourly_pay: hourlyPay,
+          end_date: endDate,
         };
 
         if (lecturerName === "" || batchCode === "" || paymentRate === "" || noOfHours === 0 || (paymentRate === "Hourly Rate" && hourlyPay === 0)) {
